da1f31b9bf4dc9e720e3f94c07c15fcc472a1d33
danielgambera <62084650+danielgambera@users.noreply.github.com>
2024-03-10 16:46:41 -0300

    Criacao Pessoa e Contato

---
+++ AppContatos/src/main/java/br/com/ehmf/AppContatos/AppContatos/model/Contato.java
@@ -0,0 +1,95 @@
+package br.com.ehmf.AppContatos.AppContatos.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "contato")
+public class Contato {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	//0 - Telefone | 1 - Celular | 2 - Email
+	@Column(nullable = false)
+	private Integer tipoContato; 
+	
+	@Column(nullable = false, length=100)
+	private String contato;
+	
+    @ManyToOne(fetch = FetchType.EAGER, optional = true)
+    @JoinColumn(name = "pessoa_id", nullable = false)
+	private Pessoa pessoa;
+
+	public Contato() {}
+
+	public Contato(Long id, Integer tipoContato, String contato) {
+		super();
+		this.id = id;
+		this.tipoContato = tipoContato;
+		this.contato = contato;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getTipoContato() {
+		return tipoContato;
+	}
+
+	public void setTipoContato(Integer tipoContato) {
+		this.tipoContato = tipoContato;
+	}
+
+	public String getContato() {
+		return contato;
+	}
+
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", tipoContato=" + tipoContato + ", contato=" + contato + "]";
+	}
+
+	
+
+}
